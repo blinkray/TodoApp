ecbfa05b8b5740339a1965f137e8174e14c7aca0
Jim Schuman <jim.schuman@gmail>
2017-05-02 16:28:01 -0500

    todo get route for id

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+var {ObjectID} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -31,6 +33,31 @@ app.get('/todos', (req,res) => {
 	}, (e) => {
 		res.status(400).send(e);
 	});
+});
+
+app.get('/todos/:id', (req,res) => {
+	var id = req.params.id;
+	
+	if( !ObjectID.isValid(id) ) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+
+		if( !todo ) {
+			res.status(400).send();
+			return;				
+		}
+
+		res.send({
+			todo
+		});
+
+		
+	}).catch((e) => {
+		res.status(400).send();
+	});
+
 });
 
 app.listen(3000, () => {
