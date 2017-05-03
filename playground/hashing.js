82c19f7167b120806ff08a2725b051b812973674
Jim Schuman <jim.schuman@gmail>
2017-05-03 18:51:31 -0500

    hashing passwords

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,17 +1,37 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-	id: 10
-};
 
-var token = jwt.sign(data, '123abc');
+var password = "123abc!";
 
-console.log(token);
+// bcrypt.genSalt(10, (err, salt ) => {
+// 	bcrypt.hash(password,salt, (err, hash) => {
+// 		console.log(hash);
+// 	});
+// });
 
-var decoded = jwt.verify( token, '123abc');
+var hashPassword = '$2a$10$U66FicxPmaUhmPoWZp/cUuq4Y4jeK9axJVS99S3VneQxZXe2wKFmy';
 
-console.log('decoded :: ', decoded);
+
+bcrypt.compare( '123', hashPassword, (err, res) => {
+	console.log(res);
+});
+
+
+
+
+// var data = {
+// 	id: 10
+// };
+
+// var token = jwt.sign(data, '123abc');
+
+// console.log(token);
+
+// var decoded = jwt.verify( token, '123abc');
+
+// console.log('decoded :: ', decoded);
 
 // var message = 'I am user number 3';
 // var hash = SHA256(message).toString();
@@ -38,3 +58,4 @@ console.log('decoded :: ', decoded);
 // } else {
 // 	console.log('data was  changed');
 // }
+
