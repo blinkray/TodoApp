d8589c2a7154224b2240877f79be90cf5ac94a4c
Jim Schuman <jim.schuman@gmail>
2017-05-06 15:24:56 -0500

    logging in users

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
 
+
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -113,6 +115,20 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  // var user = new User(body);
+
+  User.findByCredentials( body.email, body.password ).then( (user) => {
+  	return user.generateAuthToken().then((token) => {
+  		res.header('x-auth', token).send(user);
+  	});
+  }).catch((e) => {
+  	res.status(400).send();
+  });
+
 });
 
 app.listen(port, () => {
