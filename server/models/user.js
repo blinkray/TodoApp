cf5305ecdb5521f2bf6b1192d0567f4066962946
Jim Schuman <jim.schuman@gmail>
2017-05-06 14:34:50 -0500

    add tests for users

--- server/models/user.js
+++ server/models/user.js
@@ -72,16 +72,13 @@ UserSchema.statics.findByToken = function (token) {
 UserSchema.pre('save', function (next) {
   var user = this;
 
-  if( user.isModified('password') ) {
-
-    bcrypt.genSalt(10, (err, salt ) => {
-      bcrypt.hash(user.password,salt, (err, hash) => {
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
         user.password = hash;
         next();
       });
     });
-    
-
   } else {
     next();
   }
